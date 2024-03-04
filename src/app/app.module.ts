d85b40df2999916f8a4141cccf385838ae8f375f
AMendoza011003 <amendoza@becompliance.es>
2024-03-04 17:09:43 -0600

    Alertas y componentes error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,11 @@ import { RegistrarProyectoComponent } from './componentes/registrar-proyecto/reg
 import { VerProyectoComponent } from './componentes/ver-proyecto/ver-proyecto.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { InicioComponent } from './componentes/inicio/inicio.component';
+import { RCComponent } from './componentes/rc/rc.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { ToastrModule } from 'ngx-toastr';
+import { Error404Component } from './componentes/pantallaDeError/error404/error404.component';
+import { Error403Component } from './componentes/pantallaDeError/error403/error403.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +29,10 @@ import { InicioComponent } from './componentes/inicio/inicio.component';
     VerProyectoComponent,
     LoginComponent,
     InicioComponent,
-    
+    RCComponent,
+    Error404Component,
+    Error403Component
+
   ],
   imports: [
     BrowserModule,
@@ -31,6 +40,9 @@ import { InicioComponent } from './componentes/inicio/inicio.component';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    ModalModule.forRoot(),
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -14,9 +15,10 @@ export class LoginComponent implements OnInit {
   password: string = '';
   stylesFix: any;
 
-  constructor(private router: Router, private formBuilder: FormBuilder, private authService: AuthService) { }
+  constructor(private router: Router, private formBuilder: FormBuilder, private authService: AuthService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.toastr.success('Mensaje de éxito', 'Título');
   }
 
 
@@ -26,8 +28,10 @@ export class LoginComponent implements OnInit {
     this.authService.login(this.username, this.password).subscribe((response) => {
       if (response.success) {
         this.router.navigate([this.authService.getRedirectUrl()]);
+        this.toastr.success('Mensaje de éxito', 'Título');
       } else {
         // Manejar errores de inicio de sesión
+        this.toastr.error('Mensaje de error', 'Título');
         console.log('Error de inicio de sesión');
       }
     });
